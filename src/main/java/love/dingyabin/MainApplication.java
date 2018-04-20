4c6a761b4991c6774e3fc5e113062abcec5b1fbc
dingyabin <1262027459@qq.com>
2018-04-21 01:07:47 +0800

    ..

---
+++ src/main/java/love/dingyabin/MainApplication.java
@@ -0,0 +1,19 @@
+package love.dingyabin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by MrDing
+ * Date: 2018/4/21.
+ * Time:0:31
+ *
+ * @author MrDing
+ */
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
